b1e16b759e7ce8f34940a61e844a182d21e7abc8
Semyon Pupkov <semen.pupkov@gmail.com>
2015-12-04 00:22:04 +0500

    Revert "test 2"
    This reverts commit ff153c9f0e7b21eda7be7f96184d033cdb2263a3.

--- lib/vicar/base_presenter.rb
+++ lib/vicar/base_presenter.rb
@@ -1,9 +1,18 @@
 require 'delegate'
 
 module Vicar
-  # Test comment
   class BasePresenter < SimpleDelegator
     def complex_meth
+      if 1 == 2 && 2 == 1 && 3 == 4 && 4 == 7
+        puts 1
+      end
+      
+      if 1 == 2 && 2 == 1 && 3 == 4 && 4 == 6
+        puts 1
+      end
+      if 1 == 2 && 2 == 1 && 3 == 4 && 4 == 7
+        puts 1
+      end
       if 1 == 2 && 2 == 1 && 3 == 4 && 4 == 7
         puts 1
       end
