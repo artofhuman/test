26c805cb523bd674bef0e6a83dc204937aa98126
Semyon Pupkov <semen.pupkov@gmail.com>
2015-12-03 22:54:57 +0500

    Test

--- lib/vicar/base_presenter.rb
+++ lib/vicar/base_presenter.rb
@@ -1,21 +1,9 @@
 require 'delegate'
 
 module Vicar
+  # Test preenter
   class BasePresenter < SimpleDelegator
     def complex_meth
-      if 1 == 2 && 2 == 1 && 3 == 4 && 4 == 7
-        puts 1
-      end
-      
-      if 1 == 2 && 2 == 1 && 3 == 4 && 4 == 6
-        puts 1
-      end
-      if 1 == 2 && 2 == 1 && 3 == 4 && 4 == 7
-        puts 1
-      end
-      if 1 == 2 && 2 == 1 && 3 == 4 && 4 == 7
-        puts 1
-      end
     end
   end
 end
